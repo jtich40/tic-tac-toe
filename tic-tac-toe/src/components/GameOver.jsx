12d11e42e2dd21f7bd99c0c1905d905f3d070bc4
Jared Tichacek <jared.tichacek@gmail.com>
2023-06-08 22:46:09 -0500

    added logic for handleClick and checkEndGame

--- tic-tac-toe/src/components/GameOver.jsx
+++ tic-tac-toe/src/components/GameOver.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export default function GameOver({ player, tie, handleNewGame }) {
+export default function GameOver({ winner, tie, X, handleNewGame }) {
     return (
         <div className="gameover">
-            {!tie && <h1>{player ? "Player 1" : "Player 2"} Wins!</h1>}
+            {!tie && <h1>{winner === X ? "Player 1" : "Player 2"} Wins!</h1>}
             {tie && <h1>It's a Tie!</h1>}
             <button onClick={handleNewGame}>Start New Game</button>
         </div>
--- tic-tac-toe/src/components/TicTacToe.jsx
+++ tic-tac-toe/src/components/TicTacToe.jsx
@@ -18,6 +18,7 @@ const winCombo = [
 export default function TicTacToe() {
     const [grid, setGrid] = useState(Array(9).fill(""));
     const [player, setPlayer] = useState(true);
+    const [winner, setWinner] = useState("");
     const [endGame, setEndGame] = useState(false);
     const [tie, setTie] = useState(false);
 
@@ -27,20 +28,53 @@ export default function TicTacToe() {
         setTie(false);
     }
 
+
+    function handleClick(i) {
+        const newGrid = [...grid];
+        // prevent overwriting previous moves
+        if (newGrid[i] !== "" || endGame) return;
+        // set the selected square to X or O
+        newGrid[i] = player ? X : O;
+        setGrid(newGrid);
+        setPlayer(!player);
+    }
+
+    function checkEndGame() {
+        if (!endGame) {
+            for (let i = 0; i < winCombo.length; i++) {
+                // destructure the winCombo array
+                const [a, b, c] = winCombo[i];
+                // check if the grid has a value (X or O) and if the values are equivalent
+                if (grid[a] && grid[a] === grid[b] && grid[a] === grid[c]) {
+                    setEndGame(true);
+                    // set the winner based on the value of the first square in the winning combo
+                    setWinner(grid[a]);
+                    return;
+                }
+                // check if grid is full
+                let roundTie = !grid.includes("");
+                roundTie && setTie(true) && setEndGame(true);
+            }
+        }
+    }
+    // check for end game after each move
+    checkEndGame();
+
     return (
         <div>
             <h1>Click a square to begin an epic game of Tic-Tac-Toe!</h1>
             <h2>It is {player ? "Player 1" : "Player 2"}'s turn!</h2>
             {endGame && (
                 <GameOver
-                player={player}
-                tie={tie}
-                handleNewGame={handleNewGame}
+                    winner={winner}
+                    tie={tie}
+                    X={X}
+                    handleNewGame={handleNewGame}
                 />
             )}
-            <Square 
-            squares={grid}
-            handleClick={handleClick}
+            <Square
+                squares={grid}
+                handleClick={handleClick}
             />
         </div>
     )
